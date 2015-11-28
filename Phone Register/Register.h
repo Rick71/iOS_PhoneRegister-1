1f5f8163dcd343304c90b3504e0fdc452fb4e95e
Walter Gonzalez Domenzain <walter.gonzalez@smartplace.mx>
2015-11-28 08:46:40 -0600

    Customized button for phone login added

--- Phone Register/Register.h
+++ Phone Register/Register.h
@@ -9,7 +9,9 @@
 #import <UIKit/UIKit.h>
 
 @interface Register : UIViewController
+@property (strong, nonatomic) IBOutlet UILabel *lblPhone;
 
 
+- (IBAction)btnLoginPressed:(id)sender;
 @end
 
--- Phone Register/Register.m
+++ Phone Register/Register.m
@@ -8,16 +8,28 @@
 
 #import "Register.h"
 #import <DigitsKit/DigitsKit.h>
+#import "Conekta.h"
 
 @interface Register ()
 
 @end
 
 @implementation Register
-
+/*************************************/
+#pragma mark - Initialization
+/*************************************/
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [[Digits sharedInstance] logOut];
     // Do any additional setup after loading the view, typically from a nib.
+}
+//-----------------------------------------------
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+//-----------------------------------------------
+- (void)initDigits {
     DGTAuthenticateButton *authButton;
     authButton = [DGTAuthenticateButton buttonWithAuthenticationCompletion:^(DGTSession *session, NSError *error) {
         if (session.userID) {
@@ -35,12 +47,44 @@
     }];
     authButton.center = self.view.center;
     [self.view addSubview:authButton];
-
+    
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+//-----------------------------------------------
+- (void)initConekta {
+    Conekta *conekta = [[Conekta alloc] init];
+    
+    [conekta setDelegate: self];
+    
+    [conekta setPublicKey:@"key_DMMumvx5mZnFhD3nqYJPqRQ"];
+    
+    [conekta collectDevice];
+    
+    Card *card = [conekta.Card initWithNumber: @"4242424242424242" name: @"Julian Ceballos" cvc: @"123" expMonth: @"10" expYear: @"2018"];
+    
+    Token *token = [conekta.Token initWithCard:card];
+    
+    [token createWithSuccess: ^(NSDictionary *data) {
+        NSLog(@"%@", data);
+    } andError: ^(NSError *error) {
+        NSLog(@"%@", error);
+    }];
 }
+/*************************************/
+#pragma mark - Action methods
+/*************************************/
+- (IBAction)btnLoginPressed:(id)sender {
+    
+    [[Digits sharedInstance] authenticateWithCompletion:^(DGTSession *session, NSError *error) {
+        // Inspect session/error objects
+       
+        if (session != nil) {
+            NSString *stPhone = session.phoneNumber;
+            self.lblPhone.text = [@"Teléfono: " stringByAppendingString:stPhone];
+        }
+        
+    [[Digits sharedInstance] logOut];
+    }];
 
+    
+}
 @end
